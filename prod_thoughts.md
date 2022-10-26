d05833d2ad2f0b9549aea322f0e64d33ff2ccb23
NegatioN <joakimrishaug@mail.com>
2022-10-26 17:08:48 +0200

    Add a class

--- prod_thoughts.md
+++ prod_thoughts.md
@@ -4,3 +4,4 @@
 to the internal representation, as this cannot be done inside ONNX atm. (e.g. userId --> embedding index)
 2. There would need to be a specific preprocessing and post-processing interface for each model-type if hosted from Scala (?)
 Arbitrary input's seem difficult to use, and the pre/post processing seem impossible to package inside ONNX.
+3. 
--- runner/src/main/scala/Main.scala
+++ runner/src/main/scala/Main.scala
@@ -1,15 +1,39 @@
 package server
 
-import ai.onnxruntime.{OnnxTensor, OrtEnvironment}
+import ai.onnxruntime.{OnnxTensor, OrtEnvironment, OrtSession}
 import ai.onnxruntime.OrtSession.SessionOptions
 
-import scala.collection.mutable
-import scala.jdk.CollectionConverters.{MapHasAsJava, SetHasAsScala}
+import scala.jdk.CollectionConverters._
 import scala.util.Random
 
-object Main extends App {
+case class Model(path: String) {
+  // If a mapping file named ${argName}.json exists in archive, use it. Else pass data through.
+  // An archive should be a .zip file consisting of 1 onnx file, and optionally several json-files mapping inputs and outputs.
+  private val env = OrtEnvironment.getEnvironment
+  val session: OrtSession = env.createSession(path, new SessionOptions())
+  private val argNames = session.getInputNames.asScala.toSeq
+  println(f"Model argnames: ${argNames.mkString(", ")}") // Currently contentId, size
 
-  //todo make average time output (?)
+  // at load_time: make a Map[String, Map[T, Y]] or something that we can lookup in `map_in`
+  val outputMap: Map[Long, String] = (0L until 10000L).map(i => i -> i.toString).toMap
+
+  def map_in(values: Seq[Long]) = {
+    argNames.zip(values).map(x => x._1 -> OnnxTensor.createTensor(env, scala.Array(x._2))).toMap.asJava
+  }
+
+  //TODO these things need to kinda be interfaces or contracts somehow.
+  def map_out(output: OrtSession.Result) = {
+    val scores = output.get(0).getValue.asInstanceOf[Array[Float]]
+    val indices = output.get(1).getValue.asInstanceOf[Array[Long]]
+    indices.zip(scores).map(x => outputMap.get(x._1) -> x._2) // todo collapse option, only return relevant.
+  }
+  def predict_and_rank(values: Seq[Long]) = {
+    //val out = session.run(args(values))
+    //out.get(0).getValue.asInstanceOf[Array[Float]]
+    map_out(session.run(map_in(values)))
+  }
+}
+object Main extends App {
   def time[R](block: => R): R = {
     val t0 = System.nanoTime()
     val result = block
@@ -18,26 +42,9 @@ object Main extends App {
     result
   }
 
-  // Load model
-  val env = OrtEnvironment.getEnvironment
-  val opts = new SessionOptions()
-  val session = env.createSession("data/model.onnx", opts)
-
-  // create inputs
-  def onnxargs(argNames: mutable.Set[String], values: Seq[Long])= {
-    argNames.zip(values).map(x => x._1 -> OnnxTensor.createTensor(env, scala.Array(x._2))).toMap.asJava
-  }
-
-  // Current inputNodeNames are just (index of item), (number of items to output)
-  val inputNodeNames = session.getInputNames.asScala
-  val inp = onnxargs(inputNodeNames, Seq(5L, 10L)) // Seq[Any] fail... for some reason.
+  val m = Model("data/model.onnx")
+  println(m.predict_and_rank(Seq(5L, 20L)).mkString(" "))
 
   // Just test some timings
-  val n = 10
-  1 to n map{_ => time(session.run(onnxargs(inputNodeNames, Seq(Random.between(0, 100), Random.between(5, 10)))))}
-
-  // do full inference and output
-  val out = session.run(inp)
-  val actualOutput: Array[Float] = out.get(0).getValue.asInstanceOf[Array[Float]]
-  println(actualOutput.mkString(" "))
+  //1 to 10 map{_ => time(m.predict_and_rank(Seq(Random.between(0, 100), Random.between(5, 10))))}
 }
